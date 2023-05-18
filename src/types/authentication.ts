e6510ef5ceda5941061e40b288342b6add0c3288
Juansu01 <thejuank0@hotmail.com>
2023-05-18 16:23:08 -0500

    Add types for Requests

---
+++ src/types/authentication.ts
@@ -0,0 +1,31 @@
+import { Request } from "hapi";
+
+interface AuthenticationPayload {
+  email: string;
+  password: string;
+}
+
+interface RegistrationPayload {
+  role: string;
+  first_name: string;
+  last_name: string;
+  phone: string;
+  email: string;
+  password: string;
+  username: string;
+  address: string;
+  gender: string;
+  birth_date: string;
+  country_id: number;
+  city: string;
+  category: string;
+  document_id: string;
+}
+
+export interface RegisterRequest extends Request {
+  payload: RegistrationPayload;
+}
+
+export interface AuthenticationRequest extends Request {
+  payload: AuthenticationPayload;
+}
