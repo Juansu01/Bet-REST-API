0cc300188ad534bdbf3e00f287acb60f18dc422e
Juansu01 <thejuank0@hotmail.com>
2023-06-02 13:30:42 -0500

    Removed unnecessary type

--- src/services/getEmailFromAccessToken.ts
+++ src/services/getEmailFromAccessToken.ts
@@ -4,7 +4,7 @@ import Boom from "@hapi/boom";
 import { MyPayload } from "../types/userEmailPayload";
 
 export const getEmailFromAccessToken = (accessToken: string): string => {
-  const secret: Secret = process.env.ACCESS_TOKEN_SECRET as Secret;
+  const secret = process.env.ACCESS_TOKEN_SECRET as Secret;
   const payload = verify(accessToken, secret, undefined) as MyPayload;
 
   if (payload) return payload.userEmail;
