4c51d412217540fb2ef3589be88300f9a22b78db
Juansu01 <thejuank0@hotmail.com>
2023-06-05 10:23:55 -0500

    Added new function that creates a JWT using hapi

--- src/services/accessTokenGenerators.ts
+++ src/services/accessTokenGenerators.ts
@@ -1,5 +1,6 @@
 import jwt, { Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
+import Jwt from "@hapi/jwt";
 
 dotenv.config();
 
@@ -8,4 +9,24 @@ export const generateAccessToken = (userEmail: string) => {
   return jwt.sign({ userEmail }, secret, {
     expiresIn: "20m",
   });
+};
+
+export const hapiJWTGenerateToken = (userEmail: string) => {
+  const secret = process.env.ACCESS_TOKEN_SECRET as string;
+  const token = Jwt.token.generate(
+    {
+      aud: "urn:audience:test",
+      iss: "urn:issuer:test",
+      userEmail,
+      sub: "user-session",
+    },
+    {
+      key: secret,
+      algorithm: "HS512",
+    },
+    {
+      ttlSec: 1200, // 20 mins
+    }
+  );
+  return token;
 };
