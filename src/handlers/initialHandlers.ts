8a8521b397317973b4c1a44cbca2910adbcece94
Juansu01 <thejuank0@hotmail.com>
2023-06-05 13:08:31 -0500

    Logging credentials for debugging purposes

--- src/handlers/initialHandlers.ts
+++ src/handlers/initialHandlers.ts
@@ -4,6 +4,6 @@ import { UserCredentials } from "../types/authentication";
 
 export const homeHandler = async (request: Request, h: ResponseToolkit) => {
   const credentials = request.auth.credentials as UserCredentials;
-  console.log(credentials);
+  console.log(credentials.role, credentials.email);
   return h.response("Welcome to the GreenRun Backend Developer Test API!");
 };
