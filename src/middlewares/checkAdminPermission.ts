48b6cebfee344a7f08590e8bebd944e410e2fb53
Juansu01 <thejuank0@hotmail.com>
2023-06-06 13:36:43 -0500

    Middleware now checks based off user credentials. Warning: This means that whenever it is checked for the role, you need to use the jwt auth strategy, otherwise the app will fail

--- src/middlewares/checkAdminPermission.ts
+++ src/middlewares/checkAdminPermission.ts
@@ -1,29 +1,14 @@
 import { ResponseToolkit, ServerMethod, Request } from "hapi";
-import { decode, JwtPayload } from "jsonwebtoken";
 import Boom from "@hapi/boom";
 
-import { User } from "../entities/User";
-
-import { MyPayload } from "../types/userEmailPayload";
+import { UserCredentials } from "../types/authentication";
 
 export const checkAdminPermissions: ServerMethod = async (
   request: Request,
   h: ResponseToolkit
 ) => {
-  try {
-    const accessToken = request.query.access_token as string;
+  const userCredentials = request.auth.credentials as UserCredentials;
+  if (userCredentials.role === "admin") return h.continue;
 
-    if (!accessToken) throw Boom.notFound("Please provide an access token.");
-
-    const payload = decode(accessToken) as MyPayload;
-    const userToCheck = await User.findOne({
-      where: { email: payload.userEmail },
-    });
-
-    if (userToCheck?.role === "admin") return h.continue;
-
-    return Boom.forbidden("You are not an admin.");
-  } catch (err) {
-    throw Boom.badImplementation(err);
-  }
+  throw Boom.unauthorized("You are not an admin.");
 };
