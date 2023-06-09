256b309435cc1b6e6c3be0271429c5fc9096196d
Juansu01 <thejuank0@hotmail.com>
2023-06-09 14:39:44 -0500

    Added validation for register payload

--- src/routes/authenticationRoute.ts
+++ src/routes/authenticationRoute.ts
@@ -1,4 +1,5 @@
 import { ReqRefDefaults, ServerRoute } from "@hapi/hapi";
+import Joi from "joi";
 
 import {
   registerHandler,
@@ -12,6 +13,31 @@ export const authenticationRoutes: ServerRoute<ReqRefDefaults>[] = [
     method: "POST",
     path: "/api/register",
     handler: registerHandler,
+    options: {
+      validate: {
+        payload: Joi.object({
+          first_name: Joi.string().required(),
+          last_name: Joi.string().required(),
+          phone: Joi.string().required(),
+          email: Joi.string()
+            .required()
+            .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+            .messages({
+              "string.pattern.base": "Email must have a correct format.",
+            }),
+          password: Joi.string().min(8).required(),
+          username: Joi.string().alphanum().required(),
+          address: Joi.string().required(),
+          gender: Joi.string().valid("male", "female", "other").required(),
+          birth_date: Joi.date().iso().required(),
+          country_id: Joi.number().integer().required(),
+          city: Joi.string().required(),
+          category: Joi.string().valid("general", "special").required(),
+          document_id: Joi.string().required(),
+          balance: Joi.number().required(),
+        }),
+      },
+    },
   },
   {
     method: "POST",
