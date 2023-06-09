cfa584f2dd5076846516e1d306f47175c9683445
Juansu01 <thejuank0@hotmail.com>
2023-06-09 16:35:09 -0500

    Added validation to POST match

--- src/routes/matchRoute.ts
+++ src/routes/matchRoute.ts
@@ -1,7 +1,8 @@
 import { ReqRefDefaults, ServerRoute } from "@hapi/hapi";
+import Joi from "joi";
 
-import { checkAccessToken } from "../middlewares/checkAccessToken";
 import { createNewMatch, getAllMatches } from "../handlers/matchHandler";
+import { customErrorHandler } from "../services/customJoiErrorHandler";
 
 export const matchRoutes: ServerRoute<ReqRefDefaults>[] = [
   {
@@ -10,6 +11,20 @@ export const matchRoutes: ServerRoute<ReqRefDefaults>[] = [
     handler: createNewMatch,
     options: {
       auth: "jwt",
+      validate: {
+        payload: Joi.object({
+          date: Joi.date().required().error(customErrorHandler),
+          event_id: Joi.number()
+            .integer()
+            .required()
+            .messages({
+              "any.required": "Must include event id.",
+              "number.base": "Event id must be an integer.",
+            })
+            .error(customErrorHandler),
+          winner: Joi.string().optional(),
+        }),
+      },
     },
   },
   {
