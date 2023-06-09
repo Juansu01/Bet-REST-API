68d6085fd1dab03d937fb140c2f8975256ac163f
Juansu01 <thejuank0@hotmail.com>
2023-06-09 14:40:30 -0500

    Logging error codes

--- src/services/customJoiErrorHandler.ts
+++ src/services/customJoiErrorHandler.ts
@@ -1,5 +1,6 @@
 import { ValidationErrorFunction } from "joi";
 
 export const customErrorHandler: ValidationErrorFunction = (errors): string => {
+  errors.forEach((error) => console.log(error.code));
   return errors[0].message;
 };
