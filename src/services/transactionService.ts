bf5b8c5dd2c8fa501dfe11e26a82cb5634835c1e
Juansu01 <thejuank0@hotmail.com>
2023-06-10 07:23:36 -0500

    Removed try catch to fit the return types.

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,16 +1,28 @@
 import { TransactionCategory } from "../entities/Transaction";
 import { User } from "../entities/User";
 import { Transaction } from "../entities/Transaction";
-import Boom from "@hapi/boom";
 
 export const makeTransaction = async (
   depositType: TransactionCategory,
   user: User,
   amount: number
 ): Promise<string | Transaction> => {
-  try {
-    if (depositType === "deposit" || depositType === "winning") {
-      user.balance = +user.balance + amount;
+  if (depositType === "deposit" || depositType === "winning") {
+    user.balance = +user.balance + amount;
+    await user.save();
+    const newTransaction = Transaction.create({
+      category: depositType,
+      status: "accepted",
+      amount,
+    });
+    newTransaction.user = user;
+    await newTransaction.save();
+    return newTransaction;
+  }
+
+  if (depositType === "withdraw" || depositType === "bet") {
+    if (+user.balance === amount || +user.balance > amount) {
+      user.balance = +user.balance - amount;
       await user.save();
       const newTransaction = Transaction.create({
         category: depositType,
@@ -21,25 +33,7 @@ export const makeTransaction = async (
       await newTransaction.save();
       return newTransaction;
     }
-
-    if (depositType === "withdraw" || depositType === "bet") {
-      if (+user.balance === amount || +user.balance > amount) {
-        user.balance = +user.balance - amount;
-        await user.save();
-        const newTransaction = Transaction.create({
-          category: depositType,
-          status: "accepted",
-          amount,
-        });
-        newTransaction.user = user;
-        await newTransaction.save();
-        return newTransaction;
-      }
-      return `Cannot ${depositType}, balance is not enough.`;
-    }
-    return `${depositType} is not a supported deposit type.`;
-  } catch (err) {
-    console.error(err);
-    throw Boom.badImplementation(err);
+    return `Cannot ${depositType}, balance is not enough.`;
   }
+  return `${depositType} is not a supported deposit type.`;
 };
