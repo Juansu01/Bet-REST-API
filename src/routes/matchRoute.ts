554ab6ebdae26dc23832550a627c01db18261fc0
Juansu01 <thejuank0@hotmail.com>
2023-06-11 17:03:22 -0500

    Removed unnecessary use of customErrorHandler

--- src/routes/matchRoute.ts
+++ src/routes/matchRoute.ts
@@ -2,7 +2,6 @@ import { ReqRefDefaults, ServerRoute } from "@hapi/hapi";
 import Joi from "joi";
 
 import { createNewMatch, getAllMatches } from "../handlers/matchHandler";
-import { customErrorHandler } from "../services/customJoiErrorHandler";
 
 export const matchRoutes: ServerRoute<ReqRefDefaults>[] = [
   {
@@ -13,15 +12,14 @@ export const matchRoutes: ServerRoute<ReqRefDefaults>[] = [
       auth: "jwt",
       validate: {
         payload: Joi.object({
-          date: Joi.date().required().error(customErrorHandler),
+          date: Joi.date().required(),
           event_id: Joi.number()
             .integer()
             .required()
             .messages({
               "any.required": "Must include event id.",
               "number.base": "Event id must be an integer.",
-            })
-            .error(customErrorHandler),
+            }),
           winner: Joi.string().optional(),
         }),
       },
