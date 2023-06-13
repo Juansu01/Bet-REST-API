72aea290b58bb76cd0f20a50150c96e9a769aa95
Juansu01 <thejuank0@hotmail.com>
2023-06-13 11:51:21 -0500

    Added new adminAccess test file: This file will test whether admins have access to protected routes and wether normal users cannot access protected routes.

---
+++ test/adminAccess.test.ts
@@ -0,0 +1,63 @@
+import Lab from "@hapi/lab";
+import { expect } from "@hapi/code";
+
+import testServer from "../src/servers/testServer";
+import { TestServer } from "../src/types/server";
+import { LogInResponsePayload } from "../src/types/test";
+import myDataSource from "../src/services/dbConnection";
+import { TestCredentials } from "../src/types/test";
+import generateBasicAuthHeader from "./utils/generateAuthHeader";
+
+const { describe, it, before, after } = (exports.lab = Lab.script());
+
+describe("Testing admin access to protected routes.", () => {
+  const adminCredentials: TestCredentials = {
+    username: "johndoe@example.com",
+    password: "password123",
+    role: "admin",
+  };
+  const userCredentials: TestCredentials = {
+    username: "johndoe4@example.com",
+    password: "password123",
+    role: "admin",
+  };
+  let adminAccessToken: string;
+  let userAccessToken: string;
+  let server: TestServer;
+
+  before(async () => {
+    server = await testServer();
+    await myDataSource.initialize();
+    const adminAuthToken = generateBasicAuthHeader(
+      adminCredentials.username,
+      adminCredentials.password
+    );
+    const res = await server.inject({
+      method: "post",
+      url: "/api/login",
+      headers: {
+        authorization: adminAuthToken,
+      },
+    });
+    const adminLogInPayload: LogInResponsePayload = JSON.parse(res.payload);
+    adminAccessToken = adminLogInPayload.access_token;
+  });
+
+  after(async () => {
+    await server.stop();
+    await myDataSource.destroy();
+  });
+
+  it("Admin can get all transactions", async () => {
+    const res = await server.inject({
+      method: "get",
+      url: "/api/transactions",
+      headers: {
+        authorization: `Bearer ${adminAccessToken}`,
+      },
+    });
+    const json = JSON.parse(res.payload);
+    expect(Array.isArray(json)).to.equal(true);
+    expect(res.statusCode).to.equal(200);
+  });
+});
