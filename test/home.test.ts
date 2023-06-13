e83dd047e57e67240f7470e10807cc57f9c0662d
Juansu01 <thejuank0@hotmail.com>
2023-06-13 11:15:36 -0500

    Applied several changes to home test: - No longer imports base64 - Uses before and after instead of beforeEach and afterEach - Uses new generateAuthHeader function for authentication - Better test descriptions

--- test/home.test.ts
+++ test/home.test.ts
@@ -1,22 +1,22 @@
 import Lab from "@hapi/lab";
 import { expect } from "@hapi/code";
-import base64 from "base-64";
 
 import testServer from "../src/servers/testServer";
 import { TestServer } from "../src/types/server";
 import { LogInResponsePayload } from "../src/types/test";
 import myDataSource from "../src/services/dbConnection";
+import generateBasicAuthHeader from "./utils/generateAuthHeader";
 
-const { afterEach, beforeEach, describe, it } = (exports.lab = Lab.script());
+const { describe, it, before, after } = (exports.lab = Lab.script());
 
-describe("Test for home route.", () => {
+describe("Test for home route access.", () => {
   let server: TestServer;
   let accessToken: string;
   const username = "johndoe@example.com";
   const password = "password123";
-  const authHeader = "Basic " + base64.encode(`${username}:${password}`);
+  const authHeader = generateBasicAuthHeader(username, password);
 
-  beforeEach(async () => {
+  before(async () => {
     server = await testServer();
     await myDataSource.initialize();
     const res = await server.inject({
@@ -30,12 +30,12 @@ describe("Test for home route.", () => {
     accessToken = payloadToJSON.access_token;
   });
 
-  afterEach(async () => {
+  after(async () => {
     await server.stop();
     await myDataSource.destroy();
   });
 
-  it("User can access home when providing an access token.", async () => {
+  it("User can access home when providing a valid access token.", async () => {
     const res = await server.inject({
       method: "get",
       url: "/",
@@ -43,6 +43,7 @@ describe("Test for home route.", () => {
         authorization: `Bearer ${accessToken}`,
       },
     });
+    expect(res.statusCode).to.equal(200);
     expect(res.payload).to.equal(
       "Welcome to the GreenRun Backend Developer Test API!"
     );
