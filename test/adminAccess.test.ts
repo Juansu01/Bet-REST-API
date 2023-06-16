d338c57967be7b1f9c3acf05ef6ba23e52bb9547
Juansu01 <thejuank0@hotmail.com>
2023-06-16 14:35:39 -0500

    Test: Admin can get placed bet using id

--- test/adminAccess.test.ts
+++ test/adminAccess.test.ts
@@ -238,4 +238,17 @@ describe("Testing admin access to protected routes.", () => {
     expect(res.statusCode).to.equal(200);
     expect(Array.isArray(json)).to.equal(true);
   });
+  it("Admin can get placed bet using id", async () => {
+    const placedBetId = 1;
+    const res = await server.inject({
+      method: "get",
+      url: `/api/placed-bet/${placedBetId}`,
+      headers: {
+        authorization: `Bearer ${adminAccessToken}`,
+      },
+    });
+    const json = JSON.parse(res.payload);
+    expect(res.statusCode).to.equal(200);
+    expect(json).to.contain(["user_id", "bet_id", "bet_option", "amount"]);
+  });
 });
