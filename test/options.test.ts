838d1f118103656dc0905ab67847785d0ad4e0ea
Juansu01 <thejuank0@hotmail.com>
2023-06-19 14:44:07 -0500

    Add new option test file with initial test: - User cannot get all options

---
+++ test/options.test.ts
@@ -0,0 +1,54 @@
+import Lab from "@hapi/lab";
+import { expect, fail } from "@hapi/code";
+
+import testServer from "../src/servers/testServer";
+import { TestServer } from "../src/types/server";
+import myDataSource from "../src/services/dbConnection";
+import { TestCredentials } from "../src/types/test";
+import logUserIn from "./utils/logUserIn";
+import { Option } from "../src/entities/Option";
+
+const lab = Lab.script();
+const { describe, it, before, after } = lab;
+export { lab };
+
+describe("Testing option route.", () => {
+  const userCredentials: TestCredentials = {
+    username: "johndoe4@example.com",
+    password: "password123",
+    role: "user",
+  };
+  let userAccessToken: string | null;
+  let server: TestServer;
+  let willSkip = false;
+
+  before(async () => {
+    server = await testServer();
+    await myDataSource.initialize();
+    userAccessToken = await logUserIn(userCredentials, server);
+    if (userAccessToken === null) willSkip = true;
+  });
+
+  after(async () => {
+    await server.stop();
+    await myDataSource.destroy();
+  });
+
+  it("User cannot get all options.", async () => {
+    if (willSkip) fail("Wrong user credentials, test automatically failed.");
+    const res = await server.inject({
+      method: "get",
+      url: "/api/options",
+      headers: {
+        authorization: `Bearer ${userAccessToken}`,
+      },
+    });
+    const json = JSON.parse(res.payload);
+    expect(res.statusCode).to.equal(401);
+    expect(json).to.contain({
+      statusCode: 401,
+      error: "Unauthorized",
+      message: "You are not an admin.",
+    });
+  });
+});
