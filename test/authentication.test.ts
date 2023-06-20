f7545d2068b2785ba6dc069b04263be06c6ba5fc
Juansu01 <thejuank0@hotmail.com>
2023-06-20 14:32:05 -0500

    Test: User cannot block other users

--- test/authentication.test.ts
+++ test/authentication.test.ts
@@ -6,13 +6,14 @@ import { TestServer } from "../src/types/server";
 import { TestCredentials, LogInResponsePayload } from "../src/types/test";
 import myDataSource from "../src/services/dbConnection";
 import generateBasicAuthHeader from "./utils/generateAuthHeader";
+import logUserIn from "./utils/logUserIn";
 
 const { after, before, describe, it } = (exports.lab = Lab.script());
 
 describe("Test for log in route.", () => {
   let server: TestServer;
   const validUser: TestCredentials = {
-    username: "johndoe@example.com",
+    username: "johndoe4@example.com",
     password: "password123",
   };
   const invalidUser: TestCredentials = {
@@ -87,5 +88,19 @@ describe("Test for log in route.", () => {
     });
     expect(json).to.not.contain({ message: "Logged in successfully!" });
     expect(json).to.not.contain("access_token");
+  });
+  it("User cannot block other users", async () => {
+    const userId = 21;
+    const userAccessToken = await logUserIn(validUser, server);
+    const res = await server.inject({
+      method: "patch",
+      url: `/api/users/block-user/${userId}`,
+      headers: {
+        authorization: `Bearer ${userAccessToken}`,
+      },
+    });
+    const json = JSON.parse(res.payload);
+    expect(res.statusCode).to.equal(401);
+    expect(json).to.contain({ message: "You are not an admin." });
   });
 });
