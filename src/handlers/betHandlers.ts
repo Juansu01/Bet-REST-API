f21ae1e8569705850714034259128775baaf13ea
Juansu01 <thejuank0@hotmail.com>
2023-06-20 15:58:05 -0500

    Remove unused import

--- src/handlers/betHandlers.ts
+++ src/handlers/betHandlers.ts
@@ -9,7 +9,6 @@ import { makeTransaction } from "../services/transactionService";
 import { TransactionCategory } from "../entities/Transaction";
 import { Match } from "../entities/Match";
 import { BetStatus } from "../entities/Bet";
-import { Option } from "../entities/Option";
 
 export const createNewBet = async (request: BetRequest, h: ResponseToolkit) => {
   const { match_id, result } = request.payload;
