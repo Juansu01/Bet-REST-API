ca15539d8ae110d4db0dc6ccf9755a02073e725c
Juansu01 <thejuank0@hotmail.com>
2023-06-23 16:09:01 -0500

    Test: Provided access token doesnt work after being revoked

---
+++ test/logOut.test.ts
@@ -0,0 +1,58 @@
+import Lab from "@hapi/lab";
+import { expect } from "@hapi/code";
+
+import testServer from "../src/servers/testServer";
+import { TestServer } from "../src/types/server";
+import { TestCredentials } from "../src/types/test";
+import myDataSource from "../src/services/dbConnection";
+import logUserIn from "./utils/logUserIn";
+import redisClient from "../src/cache/redisClient";
+
+const { after, before, describe, it } = (exports.lab = Lab.script());
+
+describe("Test for log out route.", () => {
+  let server: TestServer;
+  const validUser: TestCredentials = {
+    username: "johndoe4@example.com",
+    password: "password123",
+  };
+  before(async () => {
+    server = await testServer();
+    await redisClient.connect();
+    await myDataSource.initialize();
+  });
+
+  after(async () => {
+    await server.stop();
+    await redisClient.quit();
+    await myDataSource.destroy();
+  });
+
+  it("Access token gets revoked after logging out", async () => {
+    const accessToken = await logUserIn(validUser, server);
+    const homeRes = await server.inject({
+      method: "get",
+      url: "/",
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+    expect(homeRes.statusCode).to.equal(200);
+    const logOutRes = await server.inject({
+      method: "delete",
+      url: "/api/logout",
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+    expect(logOutRes.statusCode).to.equal(200);
+    const secondHomeRes = await server.inject({
+      method: "get",
+      url: "/",
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+    expect(secondHomeRes.statusCode).to.equal(401);
+  });
+});
