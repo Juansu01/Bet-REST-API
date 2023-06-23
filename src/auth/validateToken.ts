5282bec22e3d397feb1d8390eba8207145c1edc5
Juansu01 <thejuank0@hotmail.com>
2023-06-23 15:16:53 -0500

    validateToken now checks if it is revoked

--- src/auth/validateToken.ts
+++ src/auth/validateToken.ts
@@ -1,10 +1,10 @@
 import { ResponseToolkit } from "hapi";
 import Jwt, { HapiJwt } from "@hapi/jwt";
 import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
 
 import { AuthenticationRequest } from "src/types/authentication";
 import { DecodedToken } from "../types/authentication";
+import isTokenRevoked from "../cache/checkTokenStatus";
 
 dotenv.config();
 export const validateToken = async (
@@ -13,10 +13,18 @@ export const validateToken = async (
   h: ResponseToolkit
 ) => {
   const secret = process.env.ACCESS_TOKEN_SECRET as string;
+  const isAccessTokenRevoked = await isTokenRevoked(artifacts.token);
+
+  if (isAccessTokenRevoked) return { isValid: false };
+
   try {
     Jwt.token.verify(artifacts, secret);
     const decoded = artifacts.decoded as DecodedToken;
-    return { isValid: true, email: decoded.email, role: decoded.role };
+    return {
+      isValid: true,
+      email: decoded.email,
+      role: decoded.role,
+    };
   } catch (err) {
     console.error(err);
     return { isValid: false };
