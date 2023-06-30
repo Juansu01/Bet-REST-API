0f088fb9e547f0e033b3d10a268e82d0129b4356
Juansu01 <thejuank0@hotmail.com>
2023-06-30 14:58:59 -0500

    Add new Event test file: It cotains one test that tests the user being able to fetch all events.

---
+++ test/event.test.ts
@@ -0,0 +1,62 @@
+import Lab from "@hapi/lab";
+import { expect, fail } from "@hapi/code";
+
+import testServer from "../src/servers/testServer";
+import { TestServer } from "../src/types/server";
+import myDataSource from "../src/services/dbConnection";
+import { TestCredentials } from "../src/types/test";
+import logUserIn from "./utils/logUserIn";
+import { Event } from "../src/entities/Event";
+import redisClient from "../src/cache/redisClient";
+
+const lab = Lab.script();
+const { describe, it, before, after } = lab;
+export { lab };
+
+describe("Testing match route.", () => {
+  const userCredentials: TestCredentials = {
+    username: "johndoe4@example.com",
+    password: "password123",
+    role: "user",
+  };
+  let userAccessToken: string | null;
+  let server: TestServer;
+  let willSkip = false;
+
+  before(async () => {
+    server = await testServer();
+    await myDataSource.initialize();
+    await redisClient.connect();
+    userAccessToken = await logUserIn(userCredentials, server);
+    if (userAccessToken === null) willSkip = true;
+  });
+
+  after(async () => {
+    await server.stop();
+    await redisClient.quit();
+    await myDataSource.destroy();
+  });
+
+  it("User can get all events.", async () => {
+    if (willSkip) fail("Wrong user credentials, test automatically failed.");
+    const res = await server.inject({
+      method: "get",
+      url: "/api/matches",
+      headers: {
+        authorization: `Bearer ${userAccessToken}`,
+      },
+    });
+    const json: Event[] = JSON.parse(res.payload);
+    expect(Array.isArray(json)).to.equal(true);
+    expect(res.statusCode).to.equal(200);
+    if (json.length > 0)
+      expect(json[0]).to.contain([
+        "id",
+        "date",
+        "event_id",
+        "bets",
+        "teams",
+        "event",
+      ]);
+  });
+});
