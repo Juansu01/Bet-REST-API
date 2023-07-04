3c2143e02904337a3c24df137465d7fc16a3049a
Juansu01 <thejuank0@hotmail.com>
2023-07-04 13:16:08 -0500

    New test: Admin can get all deleted events

--- test/event.test.ts
+++ test/event.test.ts
@@ -58,4 +58,19 @@ describe("Testing match route.", () => {
     expect(res.statusCode).to.equal(200);
     if (json.length > 0) expect(json[0]).to.contain(["id", "sport", "matches"]);
   });
+  it("Admin can get all deleted events.", async () => {
+    if (willSkip) fail("Wrong user credentials, test automatically failed.");
+    const res = await server.inject({
+      method: "get",
+      url: "/api/deleted-events",
+      headers: {
+        authorization: `Bearer ${adminAccessToken}`,
+      },
+    });
+    const json: Event[] = JSON.parse(res.payload);
+    expect(Array.isArray(json)).to.equal(true);
+    expect(res.statusCode).to.equal(200);
+    if (json.length > 0)
+      expect(json[0]).to.contain(["id", "sport", "matches", "deleted_at"]);
+  });
 });
