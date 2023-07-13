352694b27f48bfce0fff3b65be6d6cf232f9f201
Juansu01 <thejuank0@hotmail.com>
2023-07-13 14:56:28 -0500

    Test now uses logUserIn method

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -3,17 +3,14 @@ import { expect } from "@hapi/code";
 
 import testServer from "../src/servers/testServer";
 import { TestServer } from "../src/types/server";
-import {
-  LogInResponsePayload,
-  UserBalanceResponsePayload,
-} from "../src/types/test";
+import { UserBalanceResponsePayload } from "../src/types/test";
 import myDataSource from "../src/services/dbConnection";
 import { UserTestCredentials } from "../src/types/test";
-import generateBasicAuthHeader from "./utils/generateAuthHeader";
 import { TransactionPayload } from "../src/types/transaction";
 import { TransactionCategory } from "../src/entities/Transaction";
 import { Transaction } from "../src/entities/Transaction";
 import redisClient from "../src/cache/redisClient";
+import logUserIn from "./utils/logUserIn";
 
 const { describe, it, before, after } = (exports.lab = Lab.script());
 
@@ -23,28 +20,14 @@ describe("Testing transaction route.", () => {
     password: "password123",
     role: "user",
   };
-  let userAccessToken: string;
+  let userAccessToken: string | null;
   let server: TestServer;
 
   before(async () => {
     server = await testServer();
     await redisClient.connect();
     await myDataSource.initialize();
-    const userAuthToken = generateBasicAuthHeader(
-      userCredentials.username,
-      userCredentials.password
-    );
-
-    const userRes = await server.inject({
-      method: "post",
-      url: "/api/login",
-      headers: {
-        authorization: userAuthToken,
-      },
-    });
-
-    const userLogInPayload: LogInResponsePayload = JSON.parse(userRes.payload);
-    userAccessToken = userLogInPayload.access_token;
+    userAccessToken = await logUserIn(userCredentials, server);
   });
 
   after(async () => {
