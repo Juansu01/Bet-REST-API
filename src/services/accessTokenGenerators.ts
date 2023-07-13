e1b293906759744315d6f16d9f0c5e2d6d0b56f0
Juansu01 <thejuank0@hotmail.com>
2023-07-13 14:49:24 -0500

    Remove unused method and import

--- src/services/accessTokenGenerators.ts
+++ src/services/accessTokenGenerators.ts
@@ -1,17 +1,11 @@
-import jwt, { Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
 import Jwt from "@hapi/jwt";
 
 dotenv.config();
-
-export const generateAccessToken = (userEmail: string) => {
-  const secret: Secret = process.env.ACCESS_TOKEN_SECRET as Secret;
-  return jwt.sign({ userEmail }, secret, {
-    expiresIn: "20m",
-  });
-};
-
-export const hapiJWTGenerateToken = (userEmail: string, userRole: string) => {
+export const hapiJWTGenerateToken = (
+  userEmail: string,
+  userRole: string
+): string => {
   const secret = process.env.ACCESS_TOKEN_SECRET as string;
   const token = Jwt.token.generate(
     {
